8aada841838738faecdfa48ad8a772e270d8b038
Gustavo <gusyancab@gmail.com>
2022-09-25 11:14:35 -0500

    feat: impemented echahrts

--- chart-generator/src/App.tsx
+++ chart-generator/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import Input from './components/Input';
+import bmiAgeData from './bmi-age.json'
+import * as echarts from 'echarts';
 
-const Button = () => {
+type EChartsOption = echarts.EChartsOption
+
+const Button = ({ onClick }: any) => {
   return (
     <div className="">
-      <button type="button" className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <button onClick={onClick} type="button" className="inline-flex items-center rounded-full border border-transparent bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
         </svg>
@@ -14,32 +18,117 @@ const Button = () => {
   )
 }
 
-const BMIForm = ({ isLast = false }) => {
+const BMIForm = ({ isLast = false, addExtra = () => { }, id = '', showErrorAge = false, showErrorBmi = false }) => {
   return (
     <div className="bg-white px-4 py-5 sm:p-6">
       <div className="grid grid-cols-3 gap-4">
         <Input
+          id={`${id}-age`}
           label='Age (months)'
-          value=''
-          onChange={() => { }}
+          error={showErrorAge}
         />
         <Input
+          error={showErrorBmi}
+          id={`${id}-bmi`}
           label='BMI (kg/m2)'
-          value=''
-          onChange={() => { }}
         />
-        {isLast && <Button />}
+        {isLast && <Button onClick={addExtra} />}
       </div>
     </div>
   )
 }
 function App() {
-  const [ageArray, setAgeArray] = useState([{ ageMonths: '', bmi: '' }])
+  const percentilesValue = ['P5', 'P10', 'P25', 'P50', 'P75', 'P85', 'P95']
+  const [ageArray, setAgeArray] = useState([{ showErrorAge: false, showErrorBmi: false, bmi: '', ageMonths: '', id: 1 }])
   const addObAjectToArray = () => {
-    setAgeArray([...ageArray, { ageMonths: '', bmi: '' }])
+    setAgeArray([...ageArray, { showErrorAge: false, showErrorBmi: false, bmi: '', ageMonths: '', id: ageArray.length + 1 }])
+    console.log({ ageArray })
+  }
+  const generateChartData = () => {
+    let hasErrors = false
+    const result = ageArray.reduce((prev: any, { id }) => {
+      const bmiInput: any = document.querySelector(`#item-${id}-bmi`)
+      const ageInput: any = document.querySelector(`#item-${id}-age`)
+      let age, bmi, errorBmi = false, errorAge = false
+      if (ageInput) {
+        if (ageInput.value.trim() === '') {
+          errorAge = true
+          hasErrors = true
+        }
+        age = parseInt(ageInput.value)
+        if (!(age >= 24 && age <= 240.5)) {
+          errorAge = true
+          hasErrors = true
+        }
+      }
+      if (bmiInput) {
+        if (bmiInput.value.trim() === '') {
+          errorBmi = true
+          hasErrors = true
+        } else {
+          bmi = parseFloat(bmiInput.value)
+        }
+      }
+      return [
+        ...prev,
+        {
+          showErrorAge: errorAge,
+          showErrorBmi: errorBmi,
+          ageMonths: age || '',
+          bmi: bmi || '',
+          id
+        }]
+    }, [])
+
+    const sexInput: HTMLInputElement | null = document.querySelector('[name=sex]:checked')
+    let sex = sexInput?.value
+    setAgeArray(result)
+    if (hasErrors) {
+      return
+    }
+    //@ts-ignore
+    const dataPerGender = bmiAgeData[sex]
+    // transform age to agemos
+    const xAxisData = result.reduce((prev: any, current: any) => {
+      const { ageMonths } = current
+      return [...prev, `${ageMonths}.5`]
+    }, [])
+    const yAxisData = xAxisData.map((curr: any) => percentilesValue.map((columnName) => {
+      return dataPerGender[curr][columnName]
+    }))
+    const series = yAxisData.map((data: any) => {
+      return {
+        data,
+        type: 'line',
+        areaStyle: {}
+      }
+    })
+    series.push({
+      data: result.map(({ bmi }: any) => bmi),
+      type: 'line'
+    })
+    const chartData = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: xAxisData
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series
+    }
+    const chartDom = document.getElementById('chart-dom')
+    //@ts-ignore
+    const myChart = echarts.init(chartDom)
+    let option: EChartsOption
+    //@ts-ignore
+    option = chartData
+    myChart.setOption(option)
+    console.log(JSON.stringify(chartData))
   }
   return (
-    <div className="App">
+    <div className="App px-20 py-20">
       <h1 className="text-3xl font-bold underline text-zinc-900">
         BMI Calculator
       </h1>
@@ -58,36 +147,51 @@ function App() {
               </div>
             </div>
             <div className="mt-5 md:col-span-2 md:mt-0">
-              <form action="#" method="POST">
+              <form action="#" onSubmit={(e) => e.preventDefault()}>
                 <div className="overflow-hidden shadow sm:rounded-md">
-                  <BMIForm />
-                  <div className="bg-white px-4 py-5 sm:p-6">
-                    <div className="grid grid-cols-3 gap-4">
-                      <Input
-                        label='Age (months)'
-                        value=''
-                        onChange={() => { }}
-                      />
-                      <Input
-                        label='BMI (kg/m2)'
-                        value=''
-                        onChange={() => { }}
-                      />
-                      <Button />
-                    </div>
+                  {/* Sex selection */}
+                  <div className='p-5'>
+                    <fieldset className="mt-4">
+                      <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
+                        <div className="flex items-center">
+                          <input id="male" name="sex" type="radio"
+                            defaultChecked className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500" value='1' />
+                          <label htmlFor="male" className="ml-3 block text-sm font-medium text-gray-700">Male</label>
+                        </div>
+                        <div className="flex items-center">
+                          <input id="female" name="sex" type="radio" className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500" value='2' />
+                          <label htmlFor="female" className="ml-3 block text-sm font-medium text-gray-700">Female</label>
+                        </div>
+                      </div>
+                    </fieldset>
                   </div>
-
+                  {/* sex selection end */}
+                  {ageArray.map(({ id, showErrorBmi, showErrorAge }, index) => {
+                    return (
+                      <BMIForm
+                        key={id}
+                        id={`item-${index + 1}`}
+                        isLast={index === ageArray.length - 1}
+                        addExtra={addObAjectToArray}
+                        showErrorAge={showErrorAge}
+                        showErrorBmi={showErrorBmi}
+                      />
+                    )
+                  })}
                   <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                    <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+                    <button onClick={generateChartData} type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                      Show Chart / Update Values
+                    </button>
                   </div>
                 </div>
               </form>
             </div>
           </div>
         </div>
+        <div id='chart-dom' style={{ height: 500 }}></div>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- chart-generator/src/components/Input.tsx
+++ chart-generator/src/components/Input.tsx
@@ -2,21 +2,26 @@ import { ReactElement } from "react"
 
 interface InputInterface {
   label: string
-  value: string
-  onChange: (event: any) => void
+  value?: string
+  onChange?: (event: any) => void
   placeholder?: string
+  error: boolean
+  id: string
 }
 
 const Input = ({
   label,
   value,
+  id,
+  error,
   onChange,
   placeholder
 }: InputInterface): ReactElement => {
   return (
-    <div className="relative rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600">
+    <div className={`relative rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600 ${error ? 'rounded-md border-red-300' : ''}`}>
       <label className="absolute -top-2 left-2 -mt-px inline-block bg-white px-1 text-xs font-medium text-gray-900">{label}</label>
       <input
+        id={id}
         type="text"
         value={value}
         onChange={onChange}
